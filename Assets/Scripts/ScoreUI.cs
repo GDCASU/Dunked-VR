4d0a13300e476ecb30fe2a45a23e9bc59a343d10
whatiswhatlol <bradydun29@gmail.com>
2023-11-01 19:16:42 -0700

    Integrated leaderboard kinda

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.Events;
 
-public class ScoreToLeaderboard: MonoBehaviour
+public class ScoreUI: MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI inputscore;
     [SerializeField] private TMP_InputField inputname;
 
-    public UnityEvent<string, int> submitScoreEvent;
 
+    public UnityEvent<string, int> submitScoreEvent;
+    private void Awake()
+    {
+        ScoreManager.onScoreUpdate += UpdateScore;
+    }
     public void SubmitScore()
     {
         submitScoreEvent.Invoke(inputname.text, int.Parse(inputscore.text));
+    }
+
+    public void UpdateScore(int score)
+    {
+        inputscore.text = score.ToString();
     }
 }
