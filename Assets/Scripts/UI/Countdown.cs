6236a69c3e47d977489b509732cb8929519d6f56
whatiswhatlol <bradydun29@gmail.com>
2023-11-01 21:51:44 -0700

    Alarm clock

---
+++ Assets/Scripts/UI/Countdown.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+
+    [SerializeField] private TMP_Text time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WaveManager.onUpdateTimer += UpdateTimeText;
+    }
+
+    public void UpdateTimeText(float timeremaining)
+    {
+        timeremaining = Mathf.Floor(timeremaining);
+        time.text = timeremaining.ToString();
+    }
+}
